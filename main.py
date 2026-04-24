9c5a5748292c3bd164697858aa55a94e283dd164
divyadave <divyaaus123@gmail.com>
2026-04-24 20:42:45 +1000

    Initial fastapi setup

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ def read_root():
     return {"message": "Hello Divya, backend is running"}
 
 @app.get("/greet/{name}")
-def greet(name:str):
+def greet(name: str):
     return {"message": f"Hello {name}"}
