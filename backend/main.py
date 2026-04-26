7d3d59111fd348608aa57698b3d768e4ac341dd8
divyadave <divyaaus123@gmail.com>
2026-04-26 22:17:30 +1000

    API integration

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
 app = FastAPI()
@@ -11,6 +12,18 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+class JobRequest(BaseModel):
+    resume: str
+    job_description: str
+
+@app.post("/analyze")
+def analyze_job(data: JobRequest):
+    return {
+        "tailored_resume": f"Improved resume based on:{data.job_description[:50]}...",
+        "cover_letter": f"Dear Hiring Manager, based on your job: {data.job_description[:50]}..."
+
+    }
 
 @app.get("/")
 def read_root():
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,9 @@ import './App.css'
 function App() {
  const [message, setMessage]= useState("")
  const [name, setName] = useState("")
+ const [resume, setResume]= useState("")
+ const [jobDesc, setJobDesc] = useState("")
+ const [result, setResult] = useState(null)
 
  const callAPI = async () => {
   try
@@ -17,15 +20,64 @@ function App() {
     console.error("Error fetching data:", error);
   }
  }
+ const handleSubmit = async() => {
+  try {
+  const res = await fetch("http://127.0.0.1:8001/analyze", {
+    method: "POST",
+    headers: {
+      "Content-Type":  "application/json"
+    },
+    body: JSON.stringify({
+      resume: resume,
+      job_description: jobDesc
+    })
+  });
+  const data = await res.json()
+  setResult(data)
+}
+catch(error) {
+  console.error("Error", error)
+}
+ }
 
   return (
-    <>
-      
-      
-      <input type="text" placeholder='Enter your name' value={name} onChange={(e)=> setName(e.target.value)} />
-      <button onClick={callAPI}>Call Backend API</button>
-      <h2>{message}</h2>
-    </>
+   <div style={{ padding: "20px" }}>
+      <h1>AI Job Assistant</h1>
+
+      <textarea
+        placeholder="Paste your resume"
+        value={resume}
+        onChange={(e) => setResume(e.target.value)}
+        rows={5}
+        cols={50}
+      />
+
+      <br /><br />
+
+      <textarea
+        placeholder="Paste job description"
+        value={jobDesc}
+        onChange={(e) => setJobDesc(e.target.value)}
+        rows={5}
+        cols={50}
+      />
+
+      <br /><br />
+
+      <button onClick={handleSubmit}>
+        Analyze
+      </button>
+
+      {result && (
+        <div>
+          <h2>Tailored Resume:</h2>
+          <p>{result.tailored_resume}</p>
+
+          <h2>Cover Letter:</h2>
+          <p>{result.cover_letter}</p>
+        </div>
+      )}
+    </div>
   )
 }
 
